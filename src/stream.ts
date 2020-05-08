c6b0ec20c35841de19058e13e3945a753a992e5c
Tarrence van As <tarrence13@gmail.com>
2020-05-08 09:36:40 -0400

    add fhd resolution

--- src/stream.ts
+++ src/stream.ts
@@ -11,6 +11,7 @@ export const VideoResolutions: VideoResolutions = {
   vga: { width: { ideal: 640 }, height: { ideal: 360 } },
   shd: { width: { ideal: 960 }, height: { ideal: 540 } },
   hd: { width: { ideal: 1280 }, height: { ideal: 720 } },
+  fhd: { width: { ideal: 1920 }, height: { ideal: 1090 } },
 };
 
 export interface StreamOptions extends MediaStreamConstraints {
