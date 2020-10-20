2abc79606dba2ead570fc08cdb54523c5954e5bc
Tarrence van As <tarrence13@gmail.com>
2020-10-20 23:19:03 +0000

    fix(pc): dont extend RTCPeerConnection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import Client from './client';
 import { LocalStream, RemoteStream, Constraints } from './stream';
-import { Codec } from './peerconnection';
 import { Signal, IonSFUJSONRPCSignal } from './signal';
-export { Client, Codec, LocalStream, RemoteStream, Constraints, Signal, IonSFUJSONRPCSignal };
+export { Client, LocalStream, RemoteStream, Constraints, Signal, IonSFUJSONRPCSignal };
