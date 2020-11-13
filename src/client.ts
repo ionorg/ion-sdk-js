76dcdae5f79e36f67e012e50c81b55ebc69a7011
OrlandoCo <luisorlando.co@gmail.com>
2020-11-12 23:27:48 -0600

    remove sdp munging

--- src/client.ts
+++ src/client.ts
@@ -7,7 +7,6 @@ import {
   makeRemote,
   RemoteStream,
 } from './stream';
-import * as sdpTransform from 'sdp-transform';
 
 export interface Sender {
   stream: MediaStream;
@@ -179,7 +178,7 @@ export default class Client {
   private async onNegotiationNeeded() {
     try {
       await this.pc.setLocalDescription();
-      const offer = simplifySDP(this.pc.localDescription!, this.codec);
+      const offer = this.pc.localDescription!;
       const answer = await this.signal.offer(offer);
       await this.negotiate(answer);
     } catch (err) {
@@ -200,83 +199,4 @@ export default class Client {
       }
     }
   }
-}
-
-export function simplifySDP(desc: RTCSessionDescriptionInit, codec: string) {
-  if (codec === undefined) return desc;
-
-  const DefaultPayloadTypeOpus = 111;
-  let payload = 0;
-  let rtxpayload = 0;
-  let redpayload = 0;
-  let redrtxpayload = 0;
-  const session = sdpTransform.parse(desc.sdp as string);
-
-  session.media.map((m: any) => {
-    // simplfiy audio, only keep opus
-    if (m.type === 'audio') {
-      m.payloads = DefaultPayloadTypeOpus;
-      m.rtp.map((rtpmap: any, index: any) => {
-        if (rtpmap.payload !== DefaultPayloadTypeOpus) {
-          delete m.rtp[index];
-        }
-      });
-    }
-
-    // simplfiy video, only keep as we need
-    if (m.type === 'video') {
-      m.rtp.map((rtpmap: any, index: any) => {
-        // find payload by codec
-        if (rtpmap.codec.toLowerCase() === codec.toLowerCase()) {
-          payload = rtpmap.payload;
-        }
-
-        // find red payload by codec
-        if (rtpmap.codec.toLowerCase() === 'red') {
-          redpayload = rtpmap.payload;
-        }
-      });
-
-      // delete unused rtcpfb
-      if (m.rtcpFb !== undefined) {
-        m.rtcpFb.map((rtcpfb: any, index: any) => {
-          if (rtcpfb.payload !== payload) {
-            delete m.rtcpFb[index];
-          }
-        });
-      }
-
-      // find rtx payload by apt
-      m.fmtp.map((fmtp: any) => {
-        if (fmtp.config === 'apt=' + payload) {
-          rtxpayload = fmtp.payload;
-        }
-      });
-
-      m.fmtp.map((fmtp: any) => {
-        if (fmtp.config === 'apt=' + redpayload) {
-          redrtxpayload = fmtp.payload;
-        }
-      });
-
-      // delete unused rtpmap
-      m.rtp.map((rtpmap: any, index: any) => {
-        if (rtpmap.codec.toLowerCase() === 'red' || rtpmap.codec.toLowerCase() === 'ulpfec') {
-          // skip red and ulpfec
-        } else if (rtpmap.payload !== payload && rtpmap.payload !== rtxpayload && rtpmap.payload !== redrtxpayload) {
-          delete m.rtp[index];
-        }
-      });
-
-      // delete unused fmtp
-      m.fmtp.map((fmtp: any, index: any) => {
-        if (fmtp.payload !== payload && fmtp.payload !== rtxpayload) {
-          delete m.fmtp[index];
-        }
-      });
-      m.payloads = payload + ' ' + rtxpayload;
-    }
-  });
-  desc.sdp = sdpTransform.write(session);
-  return desc;
 }
