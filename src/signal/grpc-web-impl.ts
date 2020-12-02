347779aa74da93959e9c18e6cc07baf99c585f68
cloudwebrtc <duanweiwei1982@gmail.com>
2020-12-02 19:23:13 +0800

    Use 'let' and 'const' instead of 'var'.

--- src/signal/grpc-web-impl.ts
+++ src/signal/grpc-web-impl.ts
@@ -27,45 +27,39 @@ export default class IonSFUGRPCWebSignal implements Signal {
         this.streaming.on('data', (reply: SignalReply) => {
             switch (reply.getPayloadCase()) {
                 case SignalReply.PayloadCase.JOIN:
-                    var desc = JSON.parse(Uint8ArrayToString(reply.getJoin()?.getDescription() as Uint8Array));
-                    this._event.emit('join-reply', desc);
+                    const answer = JSON.parse(Uint8ArrayToString(reply.getJoin()?.getDescription() as Uint8Array));
+                    this._event.emit('join-reply', answer);
                     break;
                 case SignalReply.PayloadCase.DESCRIPTION:
-                    var desc = JSON.parse(Uint8ArrayToString(reply.getDescription() as Uint8Array));
+                    const desc = JSON.parse(Uint8ArrayToString(reply.getDescription() as Uint8Array));
                     if (desc.type === 'offer') {
                         if (this.onnegotiate) this.onnegotiate(desc);
                     } else if (desc.type === 'answer') {
                         this._event.emit('description', desc);
                     }
+                    break;
                 case SignalReply.PayloadCase.TRICKLE:
-                    var pbTrickle = reply.getTrickle();
-                    if( pbTrickle?.getInit() !== undefined) {
-                        var candidate = JSON.parse(pbTrickle.getInit() as string);
-                        var trickle = { target: pbTrickle.getTarget(), candidate: candidate};
+                    const pbTrickle = reply.getTrickle();
+                    if (pbTrickle?.getInit() !== undefined) {
+                        const candidate = JSON.parse(pbTrickle.getInit() as string);
+                        const trickle = { target: pbTrickle.getTarget(), candidate: {candidate}};
                         if (this.ontrickle) this.ontrickle(trickle as Trickle);
                     }
                     break;
                 case SignalReply.PayloadCase.ICECONNECTIONSTATE:
-                    console.log('ice connect state' + reply.getIceconnectionstate());
-                    break;
                 case SignalReply.PayloadCase.ERROR:
-                    console.log('error: ' + reply.getError());
-                    //if (this._onerror) this._onerror(reply.getError());
                     break;
             }
         });
 
-        this.streaming.on('end' || 'status', (status?: Status | undefined) => {
-            console.log(status);
-            //if (this._onerror) this._onerror(status?.details);
-        });
+        // this.streaming.on('end' || 'status', (status?: Status | undefined) => {});
     }
 
     join(sid: string, offer: RTCSessionDescriptionInit) {
-        var request = new SignalRequest();
-        var join = new JoinRequest();
+        const request = new SignalRequest();
+        const join = new JoinRequest();
         join.setSid(sid);
-        var buffer = Uint8Array.from(JSON.stringify(offer), c => c.charCodeAt(0));
+        const buffer = Uint8Array.from(JSON.stringify(offer), c => c.charCodeAt(0));
         join.setDescription(buffer);
         request.setJoin(join);
         this.streaming.write(request);
@@ -80,8 +74,8 @@ export default class IonSFUGRPCWebSignal implements Signal {
     }
 
     trickle(trickle: Trickle) {
-        var request = new SignalRequest();
-        var pbTrickle = new pb.Trickle();
+        const request = new SignalRequest();
+        const pbTrickle = new pb.Trickle();
         pbTrickle.setInit(JSON.stringify(trickle.candidate));
         request.setTrickle(pbTrickle);
         this.streaming.write(request);
@@ -89,8 +83,8 @@ export default class IonSFUGRPCWebSignal implements Signal {
 
     offer(offer: RTCSessionDescriptionInit) {
         const id = uuidv4();
-        var request = new SignalRequest();
-        var buffer = Uint8Array.from(JSON.stringify(offer), c => c.charCodeAt(0));
+        const request = new SignalRequest();
+        const buffer = Uint8Array.from(JSON.stringify(offer), c => c.charCodeAt(0));
         request.setDescription(buffer);
         this.streaming.write(request);
 
@@ -104,8 +98,8 @@ export default class IonSFUGRPCWebSignal implements Signal {
     }
 
     answer(answer: RTCSessionDescriptionInit) {
-        var request = new SignalRequest();
-        var buffer = Uint8Array.from(JSON.stringify(answer), c => c.charCodeAt(0));
+        const request = new SignalRequest();
+        const buffer = Uint8Array.from(JSON.stringify(answer), c => c.charCodeAt(0));
         request.setDescription(buffer);
         this.streaming.write(request);
     }
@@ -120,15 +114,16 @@ export default class IonSFUGRPCWebSignal implements Signal {
         }
         this._onready = onready;
     }
+
     set onerror(onerror: (error: Event) => void) {
         this._onerror = onerror;
     }
 }
 
 function Uint8ArrayToString(dataArray: Uint8Array) {
-    var dataString = "";
-    for (var i = 0; i < dataArray.length; i++) {
-      dataString += String.fromCharCode(dataArray[i]);
+    let dataString = "";
+    for (const element of  dataArray) {
+      dataString += String.fromCharCode(element);
     }
     return dataString
 }
