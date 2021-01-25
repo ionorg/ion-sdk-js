b29006845d8064f2de00c0c38253af09fbd7f9d9
tarrencev <tarrence13@gmail.com>
2021-01-25 12:16:24 -0800

    1.5.7

--- src/client.ts
+++ src/client.ts
@@ -64,10 +64,7 @@ export default class Client {
       codec: 'vp8',
       iceServers: [
         {
-          urls: [
-            'stun:stun.l.google.com:19302',
-            'stun:stun1.l.google.com:19302',
-          ],
+          urls: ['stun:stun.l.google.com:19302', 'stun:stun1.l.google.com:19302'],
         },
       ],
     },
