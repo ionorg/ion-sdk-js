e1ff04e67d9e3b295589ec3132b55f6afb7ff287
Tarrence van As <tarrencev@users.noreply.github.com>
2021-01-25 14:09:18 -0500

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -67,9 +67,6 @@ export default class Client {
           urls: [
             'stun:stun.l.google.com:19302',
             'stun:stun1.l.google.com:19302',
-            'stun:stun2.l.google.com:19302',
-            'stun:stun3.l.google.com:19302',
-            'stun:stun4.l.google.com:19302',
           ],
         },
       ],
