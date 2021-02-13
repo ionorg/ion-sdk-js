ad0e9311e185d8fbce6ff05aebf7e7659d93f481
adwpc <adwpc@hotmail.com>
2021-02-13 15:24:46 +0800

    (feat) uid optional (#160)
    * feat(add uid for join)
    * make uid optional

--- src/signal/index.ts
+++ src/signal/index.ts
@@ -5,7 +5,8 @@ export interface Signal {
   onnegotiate?: (jsep: RTCSessionDescriptionInit) => void;
   ontrickle?: (trickle: Trickle) => void;
 
-  join(sid: string, uid: string, offer: RTCSessionDescriptionInit): Promise<RTCSessionDescriptionInit>;
+
+  join(sid: string, uid: null|string, offer: RTCSessionDescriptionInit): Promise<RTCSessionDescriptionInit>;
   offer(offer: RTCSessionDescriptionInit): Promise<RTCSessionDescriptionInit>;
   answer(answer: RTCSessionDescriptionInit): void;
   trickle(trickle: Trickle): void;
